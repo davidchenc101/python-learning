684927214342dab6daf73f295efaacc2ba1ee29d
davidchenc101 <davidchenc101@gmail.com>
2023-08-10 16:13:04 +0800

    8/10 learing list dic

---
+++ datatype.py
@@ -0,0 +1,35 @@
+# 資料：程式的基本單位
+# 數字
+3456
+3.5
+
+# 字串
+"測試中文"
+"hello would"
+
+# 布林值
+True
+False
+
+# 有順序，可動的列表 list  按鈕在Ｐ旁邊
+[3, 4, 5]
+["hello", "world"]
+
+# 有順序，不可動的列表 tuple
+(3, 4, 5)
+("hello", "world")
+
+# 集合 set (集合沒有順序概念 列表有順序)
+{3, 4, 5}
+
+# 字典 dictionary#  apple 配對到蘋果 data配對到資料
+{"apple": "蘋果", "data": "資料"}
+
+# 變數：用來儲存資料的自訂名稱
+# 變數名稱＝資料
+x = 3
+
+# print(資料)
+print(x)
+x = True  # 取代舊資料
+print(x)
---
+++ list-tuple.py
@@ -0,0 +1,27 @@
+# 有序可變動列表 list
+grades = [12, 60, 15, 70, 90]
+print(grades)
+print("列表中第一個資料＝", grades[0])
+grades[0] = 55
+print("55放入第一個資料=", grades[0])
+print("列印1號到3號=", grades[1:4])
+grades[1:4] = []
+print("刪除列表（用空取代1-3號", grades)
+grades = grades + [100, 33]
+print("原來列表加新列表＝", grades)
+
+# lenth=len(列表)
+# len(grades)
+
+# 巢狀列表
+data = [[3, 4, 5], [6, 7, 8]]  # 第0組是345,第0組的第1位是4,第0組的第2位是5
+print(data[0][1])
+
+data[0][0:2] = [5, 5, 5]  # 把3跟4換成三個5
+print(data)
+
+# 有序不可變動列表 Tuple
+data2 = (3, 4, 5)
+print("data2＝", data2)
+# 跟list差別在 打這串會error
+# data[0]=5  #不能把5放入data2[0]的位置
